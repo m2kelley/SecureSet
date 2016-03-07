e171934421f8e1fd56021e5fc5c7142ebb276077
m2kelley <michael.s.kelley.ii@icloud.com>
2016-03-07 08:16:02 -0700

    Final Draft of Take Home test.

--- sysAuthenticate/ListDir.py
+++ sysAuthenticate/ListDir.py
@@ -3,28 +3,43 @@
 Written by Michael Kelley @ SecureSet 2016
 """
 import os
-import stat
+import datetime
 
-def getuidlist(path):
+path = (input('Please specify a directory:'))
 
-    print('Origin[' + path + ']')
-    x = 0
-    y = 0
-    for root, dirs, files in os.walk(path):
-        if stat.ST_UID:
-            x += 1
-        if oct(stat.ST_MODE) > oct(0o0):
-            y += 1
-        for name in files:
-            p = os.path.join(root, name)
-            s = stat.ST_UID
-            m = oct(stat.ST_MODE)
-            instr = "'Path::{0}', 'UID::{1}', 'MODE::{2}'".format(p, s, m)
-            print(instr)
-        for name in dirs:
-            print(os.path.join(root, name))
-    print('Number of set UID bits is: %i' % x)
-    print('Number of set MODE bits is: %s' % y)
 
-if __name__ == '__main__':
-    getuidlist(input('Please specify path to scan:'))
+def dirwalk(path):
+    g = 0
+    for root, dirs, files in os.walk(path, topdown=True):
+        try:
+            for name in files:
+                name = name.strip()
+#            os.stat_result(st_mode=33188, st_ino=7876932, st_dev=234881026,
+# st_nlink=1, st_uid=501, st_gid=501, st_size=264, st_atime=1297230295,
+# st_mtime=1297230027, st_ctime=1297230027)
+                a = os.path.join(root, name)
+                statinfo = os.stat(a)
+                if statinfo.st_uid or statinfo.st_gid > 0:
+                    ctime = datetime.datetime.fromtimestamp(statinfo.st_ctime)
+                    mtime = datetime.datetime.fromtimestamp(statinfo.st_mtime)
+                    atime = datetime.datetime.fromtimestamp(statinfo.st_atime)
+                    own = statinfo.st_uid
+                    print(a, '\nUID:', own, '\ncreated:', ctime, '\nmodified:', mtime, '\nlast accessed:', atime)
+                else:
+                    g += 1
+            for name in dirs:
+                name = name.strip()
+                d = os.path.join(root, name)
+                statinfo2 = os.stat(d)
+                if statinfo2.st_uid or statinfo2.st_gid > 0:
+                    cctime = datetime.datetime.fromtimestamp(statinfo.st_ctime)
+                    mmtime = datetime.datetime.fromtimestamp(statinfo.st_mtime)
+                    aatime = datetime.datetime.fromtimestamp(statinfo.st_atime)
+                    oown = statinfo.st_uid
+                    print(d, '\nUID:', oown, '\ncreated:', cctime, '\nmodified:', mmtime, '\nlast accessed:', aatime)
+                else:
+                    g += 1
+        except FileNotFoundError:
+            continue
+    print('%i known files and directories with no UID or GID bits set.' % g)
+dirwalk(path)
